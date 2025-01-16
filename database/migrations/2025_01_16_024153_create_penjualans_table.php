b9ad5a49d115b81561c7a42ca50087a10b350881
shaaa67 <pashaadutt1@gmail.com>
2025-01-16 11:03:14 +0700

    setting migration kasir-laravel

---
+++ database/migrations/2025_01_16_024153_create_penjualans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('penjualan', function (Blueprint $table) {
+            $table->id();
+            $table->date('TanggalPenjualan');
+            $table->decimal('TotalHarga', 10, 2);
+            $table->unsignedBiginteger('UsersId'); // Corrected here
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualan');
+    }
+};
